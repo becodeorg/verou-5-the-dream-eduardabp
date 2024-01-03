928135b8819c547266759d44464bec97da75a274
Eduarda <eduardabpetry@gmail.com>
2024-01-03 10:24:10 +0100

    Create form

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real to Euro</title>
+</head>
+<body>
+    <main>
+        <h1>Real to Euro</h1>
+        <br>
+        <h2>Do you want to check how many caipirinhas you can buy in Brazil with your VDAB allowance? Calculate it here!</h2>
+        <br>
+        <form>
+            <label for="real-price">Value</label>
+            <input type="number" name="real-price" id="real-price">
+            <button type="submit">Calculate</button>
+        </form>
+    </main>    
+</body>
+</html>
